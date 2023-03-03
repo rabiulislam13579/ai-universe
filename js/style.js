898ce83ea8efd20da32ece7bf94c109d748b309d
rabiulislam13579 <rabiul120158@gmail.com>
2023-03-03 19:02:22 +0600

    card

--- js/style.js
+++ js/style.js
@@ -5,9 +5,26 @@ const loadData=()=>{
     .then(data=> displayData(data.data.tools));
 }
 const displayData=elements=>{
-    // console.log(elements);
+   const divContainer=document.getElementById('div-container');
     for(const element of elements){
-        console.log(element);
+        console.log(element)
+        const div=document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`
+        <div class="card">
+            <img src="${element.image}" class="box-img img-fluid card-img-top p-4 rounded-3" alt="...">
+            <div class="card-body">
+                <h4 class="mb-3">Features</h4>
+                <p class="card-text"><ol>
+                <li>${element.features[0]}</li>
+                <li>${element.features[1]}</li>
+                <li>${element.features[2]}</li>
+                </ol>
+                </p>
+            </div>
+        </div>
+        `
+        divContainer.appendChild(div);
     }
 
 }
